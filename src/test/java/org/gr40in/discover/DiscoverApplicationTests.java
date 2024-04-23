ba5226f724cccdfb71c1d962f5a239dd761e72e0
Richard <gr40in@yandex.ru>
2024-04-24 03:12:10 +0500

    Initial commit

---
+++ src/test/java/org/gr40in/discover/DiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gr40in.discover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
